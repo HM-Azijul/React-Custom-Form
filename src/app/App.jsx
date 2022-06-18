776ab29ca2130fad6917aae8b5e56a2735d88f88
HM-Azijul <azijulhm@gmail.com>
2022-06-18 11:08:05 +0600

    add Button, TextInput, Text component under UI components

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,16 @@
+import Button from '../components/UI/buttons/Button';
+import TextInput from '../components/UI/inputs/TextInput';
+import Text from '../components/UI/texts/Text';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <TextInput />
+      <Button>Test Me</Button>
+      <Text size="lg">Hello World</Text>
+    </div>
+  );
 };
 
 export default App;
